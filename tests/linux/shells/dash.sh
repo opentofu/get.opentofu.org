52aa1ac8d99afd40be483788b7788372304bd4cb
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2023-12-21 19:20:36 +0100

    Fixed dash

--- tests/linux/shells/dash.sh
+++ tests/linux/shells/dash.sh
@@ -3,17 +3,21 @@
 case "$DISTRO" in
   debian)
     apt-get install -y dash
+    export SHELL_COMMAND=/bin/dash
     ;;
   ubuntu)
     sudo apt-get install -y dash
+    export SHELL_COMMAND=/bin/dash
     ;;
   alpine)
     apk add dash
+    export SHELL_COMMAND=/usr/bin/dash
     ;;
   fedora | rocky)
     yum install -y dash
+    export SHELL_COMMAND=/bin/dash
     ;;
   opensuse)
     zypper install -y dash
+    export SHELL_COMMAND=/bin/dash
 esac
-export SHELL_COMMAND=/bin/dash
