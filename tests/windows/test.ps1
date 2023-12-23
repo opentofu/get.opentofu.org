abc8ec678940dd9ca54f6c72905d62f53d846ed5
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2023-12-23 10:54:26 +0100

    Windows tests
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -1,7 +1,12 @@
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$method = "auto"
+)
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
-../../src/install-opentofu.ps1
+../../src/install-opentofu.ps1 -installMethod "${method}"
 
 tofu --version
